76cf5206bd3a1370ad5bd6e3851128864e6d2c22
Aiswarya <aiswaryaaishu0001@gmail.com>
2022-09-28 14:16:16 +0530

    final updation

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from blogapp.models import UserProfile,Blogs,Comments
